1f62c858cab88f347a1e1651a36082020769fdd5
TuomasLehti <tuomas.a.lehti@gmail.com>
2017-12-12 13:46:40 +0200

    Add files via upload

---
+++ HelBDutyListReader.java
@@ -0,0 +1,11 @@
+package tuomaan.pdftest;
+
+/**
+ * An abstract class, whose descendants read a duty list from a file to memory.
+ */
+/* Version 0.1 (2017-12-08)
+ */
+
+public abstract class HelBDutyListReader {
+    abstract public HelBDutyList read();
+}
---
+++ HelBDutyListWriter.java
@@ -0,0 +1,10 @@
+package tuomaan.pdftest;
+
+/**
+ * An abstract class, whose descendants write a duty list from memory to a file.
+ */
+/* Version 0.1 (2017-12-08)
+ */
+
+public abstract class HelBDutyListWriter {
+}
