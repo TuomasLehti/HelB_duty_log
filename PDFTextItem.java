806a2b124f0a629211b02fa7967357a2d9f307fd
TuomasLehti <tuomas.a.lehti@gmail.com>
2019-03-21 15:41:26 +0200

    Includes the coordinates for the "surrounding box".
    Includes the coordinates for the "surrounding box", by which the items are now sorted. See readme.md for more information.

--- PDFTextItem.java
+++ PDFTextItem.java
@@ -1,12 +1,18 @@
-package tuomaan.pdftest;
-
+package pdftextextractor;
 /**
  * A simple class containing the data of a single text snippet from HelB's duty log pdf-files.
  *
  * @author Tuomas Lehti
- * @version 1.0
+ * @version 2019-03-21
  */
 public class PDFTextItem implements Comparable<PDFTextItem> {
+
+	// the coordinates for the surrounding box
+	public Float topLeftX;
+	public Float topLeftY;
+	public Float bottomRightX;
+	public Float bottomRightY;
+
     public Float x;
     public Float y;
     public int page;
@@ -14,19 +20,45 @@ public class PDFTextItem implements Comparable<PDFTextItem> {
 
     @Override
     public String toString() {
-        return "\"" + page + "\";\"" + x + "\";\"" + y + "\";\"" + text + "\"";
+		String separator = "\";\"";
+		StringBuilder sb = new StringBuilder();
+		sb.append("\"");
+		sb.append(page+"");
+		sb.append(separator);
+		sb.append(topLeftX+"");
+		sb.append(separator);
+		sb.append(topLeftY+"");
+		sb.append(separator);
+		sb.append(bottomRightX+"");
+		sb.append(separator);
+		sb.append(bottomRightY+"");
+		sb.append(separator);
+		sb.append(x+"");
+		sb.append(separator);
+		sb.append(y+"");
+		sb.append(separator);
+		sb.append(text);
+		sb.append("\"");
+        return sb.toString();
     }
 
     @Override
     public int compareTo(PDFTextItem other) {
         if (!(this.page==other.page)) {
             return this.page - other.page;
-        } else if (!(this.y.compareTo(other.y)==0)) {
-            return Math.round(this.y) - Math.round(other.y);
-        } else if (!(this.x.compareTo(other.x)==0)) {
-            return Math.round(this.x) - Math.round(other.x);
+        } else if (this.topLeftY.compareTo(other.topLeftY)!=0) {
+            return Math.round(this.topLeftY) - Math.round(other.topLeftY);
+        } else if (this.topLeftX.compareTo(other.topLeftX)!=0) {
+            return Math.round(this.topLeftX) - Math.round(other.topLeftX);
         } else {
             return 0;
         }
     }
 }
+/* 
+CHANGELOG
+
+2019-03-19: 
+- Includes the coordinates for the surrounding box.
+- Objects are compared by the coordinates of the surrounding box.
+*/
